5a222430ddf6cfc79dc72a265df71b6e900faaf3
AdamTasma <adamtasma@gmail.com>
2018-01-30 13:34:15 -0500

    first commit

---
+++ HelperMethod_Repeat/Program.cs
@@ -0,0 +1,23 @@
+﻿
+static void Repeat()
+{
+    Console.WriteLine("Would you like to repeat (y/n)");
+
+    if (char.TryParse(Console.ReadLine(), out char result))
+    {
+        char response = char.Parse(Console.ReadLine().ToLower());
+        if (response == 'y')
+        {
+            Main();
+        }
+        else if (response == 'n')
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+    else
+    {
+        Console.WriteLine("I'm sorry, I didn't understand. Would you like to learn about another student?(y/n)");
+        Repeat();
+    }
+}
